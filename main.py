85d951ffe4e94fd7c170bbb203f78137ed775583
Richard Marko <rmarko@fedoraproject.org>
2014-10-09 16:41:39 +0200

    keep only next_ versions

--- main.py
+++ main.py
@@ -1,93 +1,195 @@
-#!/usr/bin/env python
+#!/usr/bin/python
+import os
+import shutil
 
-import time
-import logging
-from pllm import backends, config, fuhacko
+from twisted.python import log
+from twisted.application import service
+from twisted.application.internet import TCPClient, TCPServer
+from twisted.internet import task
+from twisted.internet import reactor, threads
+from twisted.internet.defer import Deferred
 
-def main():
-    logging.basicConfig(level=logging.DEBUG)
-    #logging.basicConfig(level=logging.ERROR)
+from pllm import manhole, interpret, vision
+from pllm import backends, config, monitor
 
-    # init libvirt connection
-    lv = backends.LibvirtBackend('qemu:///system', 'lvm')
-
-    with open('data/f20.xml') as f:
-        xml = f.read()
-
-    lv.remove_test_vm('f20')
-    dom = lv.create_test_vm(xml, 'f20',
-        '/var/lib/libvirt/images/Fedora-20-x86_64-netinst.iso')
-
-    dom.start()
-    time.sleep(.5)
-
-    import cv
-    cv.NamedWindow('master')
-
-    def mousecb(event, x, y, flags, img):
-        if event == cv.CV_EVENT_LBUTTONDOWN:
-            logging.debug('Click ({0},{1})'.format(x,y))
-            logging.debug('Size ({0},{1})'.format(img.width, img.height))
-            dom.click(x, y, img.width, img.height)
-
-    import threading
-    class ScreenThread(threading.Thread):
-        def __init__(self, dom, interval=3000):
-            super(ScreenThread, self).__init__()
-            self.dom = dom
-            self.target = config.get('screenshot_target')
-            self.interval = interval
-            self.running = False
-
-            self.distinct = True
-
-        # compare doesn't work, idea was to use it to compare
-        # subsequent screenshots and save space and processing time
-        # if they are similar
-        def compare(self, im1, im2):
-            if type(im1) != type(im2):
-                return False
-
-            if im1.width != im2.width or im1.height != im2.height:
-                return False
-
-            dst = cv.CreateImage((im1.width, im2.height), cv.IPL_DEPTH_8U, 1)
-            #cv.Zero(dst)
-            cv.Cmp(im1, im2, dst, cv.CV_CMP_NE)
-            logging.debug('WAT {0}'.format(cv.CountNonZero(dst)))
-            return dst
-
-        def run(self):
-            while True:
-                if not self.running:
-                    return
+from pllm.vnc.vnc import VNCFactory
 
 
-                dom.screenshot()
-                cv.ShowImage('master', dom.screen)
-                cv.SaveImage('{0}/{1}.png'.format(self.target,
-                    dom.screen_id), dom.screen)
+def trace(func):                         # Use function, not class with __call__
+    def wrapper(*args, **kwargs):
+        print('{0}'.format(func.__name__))
+        val = func(*args, **kwargs)
+        print('/{0}'.format(func.__name__))
+        return val
+    return wrapper
 
-                cv.WaitKey(self.interval)
+CAP_DELAY = 1
 
-        def start(self):
-            self.running = True
-            super(ScreenThread, self).start()
 
-        def stop(self):
-            self.running = False
+class Pllm(object):
+    '''
+    Master state machine
+    '''
 
-    sct = ScreenThread(dom)
-    sct.start()
-    time.sleep(1)
+    def __init__(self, application):
+        super(Pllm, self).__init__()
 
-    fuh = fuhacko.Fuhacko(dom)
+        self.vnc = None
+        self.int = None
+        self.dom = None
+        self.mon = None
+        self.manhole = None
+        self.app = application
+        self.ocr_enabled = True
 
-    with open('pseudo.py') as f:
-        code = f.read()
-        fuh.start(code)
+        self.dom_ident = 'f20'
+        f = '/var/lib/libvirt/images/Fedora-20-x86_64-netinst.iso'
+        self.dom_media_path = f
+        self.libvirt_uri = config.get('libvirt_uri')
+        self.storage_pool_name = config.get('storage_pool_name')
 
-    sct.stop()
+        self.work_dir = config.get('work_dir')
+        self.state = 'INIT'
 
-if __name__ == "__main__":
-    main()
+    def main(self):
+        prev_state = self.state
+
+        if self.state == 'INIT':
+            if not self.mon:
+                self.start_monitor()
+            else:
+                self.state = 'VM_INIT'
+
+        if self.state == 'VM_INIT':
+            if not self.dom:
+                self.start_domain()
+
+            elif self.dom.is_running:
+                self.state = 'VNC_INIT'
+
+        if self.state == 'VNC_INIT':
+            if not self.vnc:
+                self.start_vnc()
+            else:
+                self.state = 'MANHOLE_INIT'
+
+        if self.state == 'MANHOLE_INIT':
+            if not self.manhole:
+                self.start_manhole()
+            else:
+                self.state = 'MAIN'
+
+        if self.state == 'MAIN':
+            if not self.int:
+                self.start_interpret()
+            else:
+                self.state = 'RUNNING'
+
+        if prev_state != self.state:
+            self.emit(self.state)
+
+        reactor.callLater(1, self.main)
+
+    def start_domain(self):
+        lv = backends.LibvirtBackend(self.libvirt_uri, self.storage_pool_name)
+        xml = 'data/{0}.xml'.format(self.dom_ident)
+
+        with open(xml) as f:
+            xml = f.read()
+
+        lv.remove_test_vm(self.dom_ident)
+        self.dom = lv.create_test_vm(xml, self.dom_ident, self.dom_media_path)
+        self.dom.start()
+
+    def start_vnc(self):
+        vnc_factory = VNCFactory()
+        vnc_factory.deferred.addCallback(self.vnc_started)
+        vnc_service = TCPClient("localhost", 5900, vnc_factory)
+        vnc_service.setServiceParent(self.app)
+
+    def start_manhole(self):
+        opts = {
+            'ssh':    1667,
+            'namespace': {'pllm': self},
+        }
+
+        self.manhole = manhole.manhole_service(opts)
+        self.manhole.setServiceParent(self.app)
+
+    def start_monitor(self):
+        self.mon = monitor.MonitorFactory()
+        monitor_service = TCPServer(1666, self.mon)
+        monitor_service.setServiceParent(self.app)
+
+    def emit(self, msg):
+        self.mon.broadcast(msg)
+
+    @trace
+    def vnc_started(self, proto):
+        #proto.framebufferUpdateRequest()
+        self.vnc_loop = task.LoopingCall(proto.framebufferUpdateRequest)
+        self.vnc_loop.start(1.0)
+
+        self.schedule_save(proto, 0)
+        self.vnc = proto
+        self.dom.transport = proto
+
+    @trace
+    def store_ocr_results(self, ocr_res):
+        full, segments = ocr_res
+        log.msg('ocr: full page text length:{0}, segments:{1}'
+                .format(len(full), len(segments)))
+
+        log.msg('ocr: full sample: "{0}..."'.format(repr(full[:30])))
+
+        self.dom.text = full
+        self.dom.segments = segments
+
+    @trace
+    def save_screen(self, proto, counter):
+        screendir = os.path.join(self.work_dir, str(counter))
+        if os.path.isdir(screendir):
+            shutil.rmtree(screendir)
+
+        os.mkdir(screendir)
+
+        fpath = os.path.join(screendir, "screen.png")
+        cpath = os.path.join(self.work_dir, "last.png")
+        proto.save_screen(fpath)
+        proto.save_screen(cpath)
+
+        if self.ocr_enabled:
+            d = threads.deferToThread(vision.process, fpath)
+            d.addCallback(self.store_ocr_results)
+
+        with self.dom.screen_lock:
+            self.dom.screen = proto.screen
+            self.dom.screen_id = counter
+
+        reactor.callLater(CAP_DELAY, self.schedule_save, proto, counter + 1)
+
+    @trace
+    def schedule_save(self, proto, counter):
+        proto.deferred = Deferred()
+        proto.deferred.addCallback(self.save_screen, counter)
+
+    @trace
+    def start_interpret(self):
+        with open('next_pseudo.py') as f:
+            code = f.read()
+
+        self.int = interpret.Interpret(self.dom)
+        reactor.callInThread(self.int.start, code)
+
+    @trace
+    def stop(self):
+        # stop bdb interpreter
+        if self.int:
+            self.int.set_quit()
+
+
+application = service.Application("PLLM")
+app = Pllm(application)
+# yield so logging and twistd stuff can be initialized
+reactor.callLater(0.1, app.main)
+reactor.addSystemEventTrigger('during', 'shutdown', app.stop)
--- pseudo.py
+++ pseudo.py
@@ -1,7 +1,7 @@
 import time
-import logging
 
-from pllm import process, config, util
+from pllm import vision, config, util
+
 
 class screenlock(object):
     def __init__(self, func):
@@ -11,6 +11,7 @@ class screenlock(object):
         with dom.screen_lock:
             res = self.func(dom, template)
         return res
+
 
 class cachefind(object):
     def __init__(self, func):
@@ -31,146 +32,275 @@ class cachefind(object):
 
         return res
 
+
 @screenlock
 @cachefind
 def find(dom, template):
-    res, x, y = process.match(dom.screen, util.load_img(template))
+    res, x, y = vision.template_match(dom.screen, util.load_img(template))
     if res > config.get('treshold'):
-        logging.debug('+{0}@{1} = {2}'.format(template, dom.screen_id, res))
+        print('+{0}@{1} = {2}'.format(template, dom.screen_id, res))
         ret = True
     else:
-        logging.debug('-{0}@{1} = {2}'.format(template, dom.screen_id, res))
+        print('-{0}@{1} = {2}'.format(template, dom.screen_id, res))
         ret = False
 
     return ret
 
+
 @screenlock
 @cachefind
 def findxy(dom, template):
-    res, x, y = process.match(dom.screen, util.load_img(template))
+    res, x, y = vision.template_match(dom.screen, util.load_img(template))
     if res > config.get('treshold'):
-        logging.debug('+{0}@{1} = {2}'.format(template, dom.screen_id, res))
+        print('+{0}@{1} = {2}'.format(template, dom.screen_id, res))
         ret = True
     else:
-        logging.debug('-{0}@{1} = {2}'.format(template, dom.screen_id, res))
+        print('-{0}@{1} = {2}'.format(template, dom.screen_id, res))
         ret = False
 
     return (ret, x, y)
 
+
 def expect(dom, items):
     for stage_name, template, callback in items:
-        logging.debug('Testing stage {0}'.format(stage_name))
+        print('Testing stage {0}'.format(stage_name))
         if find(dom, template):
-            logging.debug('Stage {0} found'.format(stage_name))
+            print('Stage "{0}" found'.format(stage_name))
             callback(dom)
 
+
+def expect_text(dom, items):
+    for stage_name, text, callback in items:
+        print('Testing text presence "{0}" for stage "{1}"'
+              .format(text, stage_name))
+
+        if text.lower() in dom.text.lower():
+            print('Stage "{0}" found'.format(stage_name))
+            callback(dom)
+
+
 def click(dom, template):
-    logging.debug('Looking for click target')
-    ret, x ,y = findxy(dom, template)
+    print('Looking for click target')
+    ret, x, y = findxy(dom, template)
 
     if ret:
-        dom.click(x, y)
+        dom.clickxy(x, y)
     else:
-        logging.debug('Click target not found')
+        print('Click target not found')
 
     time.sleep(0.5)
 
-def wait(dom, template, timeout_seconds=20):
+
+def click_text(dom, text):
+    print('Looking for click target with text "{0}"'.format(text))
+
+    for segname, data in dom.segments.items():
+        rect, ocrd = data
+        x, y, w, h = rect
+
+        if text.lower() in ocrd.lower():
+            print('Found target at {0}x{1}'.format(x, y))
+            dom.clickxy(x + w / 2, y + h / 2)
+            return
+
+    print('Click target not found')
+
+
+def wait(dom, template, timeout_seconds=30):
     start = time.time()
-    logging.debug('Waiting for target for {0} sec'.format(timeout_seconds))
+    print('Waiting for target for {0} sec'.format(timeout_seconds))
     while True:
         if find(dom, template):
-            logging.debug('Wait target found after {:.2} sec'.format(
+            print('Wait target found after {:.2} sec'.format(
                 time.time() - start))
             return True
 
         time.sleep(0.1)
 
         if time.time() > start + timeout_seconds:
-            logging.debug('!! Timeout on wait')
+            print('!! Timeout on wait')
             return False
 
-def waitclick(dom, template):
+
+def wait_text(dom, text, timeout_seconds=30):
+    start = time.time()
+    print('Waiting for "{0}" text for {1} sec'.format(text, timeout_seconds))
+
+    while True:
+        if text.lower() in dom.text.lower():
+            print('Wait target found after {:.2} sec'.format(
+                time.time() - start))
+            return True
+
+        # check segments
+        for segname, data in dom.segments.items():
+            rect, ocrd = data
+            x, y, w, h = rect
+
+            if text.lower() in ocrd.lower():
+                print('Wait target found at segment {0}x{1} after{:.2} sec'
+                      .format(x, y, time.time() - start))
+                return True
+
+        time.sleep(0.1)
+
+        if time.time() > start + timeout_seconds:
+            print('!! Timeout on wait')
+            return False
+
+
+def wait_click(dom, template):
     wait(dom, template)
     click(dom, template)
 
+
+def wait_click_text(dom, text):
+    wait_text(dom, text)
+    click_text(dom, text)
+
+
 def grub(dom):
-    dom.send_key('ret')
+    dom.key_press('tab')
+    # erase part of cmdline
+    for _ in "rd.live.check quiet":
+        dom.key_press('backspace')
+        time.sleep(0.2)
+
+    # disable gelocation so we don't start with random language
+    dom.write(' geoloc=0')
+    time.sleep(2)  # screenshot
+    dom.key_press('ret')
+
 
 def anaconda(dom):
-    time.sleep(1)
-    click(dom, 'anaconda_continue')
-    wait(dom, 'anaconda_my_fate', 6)
-    if find(dom, 'anaconda_eng_lang'):
-        logging.warning('First click glitch present (Found eng lang)')
-        click(dom, 'anaconda_eng_lang')
-        click(dom, 'anaconda_continue')
+    # first click glitch, but why?
+    dom.mouse_move(1, 1)
+    #dom.click()
 
-    waitclick(dom, 'anaconda_my_fate')
-    waitclick(dom, 'anaconda_storage_btn')
-    waitclick(dom, 'anaconda_continue')
-    waitclick(dom, 'anaconda_dialog_continue')
+    click_text(dom, 'continue')
+    if not wait_text(dom, 'installation summary', 5):
+        print('First click glitch present')
+        click_text(dom, 'continue')
 
-    waitclick(dom, 'anaconda_software_selection_btn')
-    #waitclick(dom, 'anaconda_xfce_choice')
-    waitclick(dom, 'anaconda_done_btn')
+    wait_text(dom, 'installation summary')
 
-    waitclick(dom, 'anaconda_begin_btn')
-    waitclick(dom, 'anaconda_bluekey_img')
-    wait(dom, 'anaconda_done_btn')
-    for _ in [1,2]:
+    while True:
+        wait_click(dom, 'anaconda_storage_incomplete_btn')
+        if wait_text(dom, 'local standard disks'):
+            break
+
+    wait_click_text(dom, 'done')
+    wait_text(dom, 'installation options')
+    dom.key_press('alt-m')
+    wait_click_text(dom, 'continue')
+
+    def create_partition(mount, size):
+        wait_text(dom, 'manual partitioning')
+
+        while not wait_text(dom, 'add a new mount point', 2):
+            wait_click(dom, 'anaconda_partitioning_plus_btn')
+
+        dom.write(mount)
+        dom.key_press('right')
+        dom.key_press('tab')
+        dom.key_press('tab')
+        dom.write(size)
+
+        while not wait_text(dom, 'manual partitioning', 2):
+            wait_click_text(dom, 'add mount point')
+
+    create_partition('/', '5G')
+    create_partition('/boot', '100M')
+    create_partition('/home', '1G')
+
+    wait_click_text(dom, 'done')
+    wait_click_text(dom, 'accept changes')
+
+    wait_click(dom, 'anaconda_software_btn')
+    #wait_click(dom, 'anaconda_xfce_choice')
+    wait_click_text(dom, 'done')
+    wait_click_text(dom, 'begin installation')
+
+    wait_click(dom, 'anaconda_rootpw_btn')
+    wait_text(dom, 'root password')
+    for _ in [1, 2]:
         for i in range(6):
-            dom.send_key(str(i))
+            dom.key_press(str(i))
 
-        dom.send_key('tab')
+        dom.key_down('tab')
 
+    # password too simple, need to click two times
+    wait_click_text(dom, 'done')
+    wait_click_text(dom, 'done')
+
+    time.sleep(2)
+    print(dom.text)
+
+    wait_click(dom, 'anaconda_user_creation_btn')
+    wait_click_text(dom, 'done')
+
+    time.sleep(2)
+    print(dom.text)
+
+    '''
+    wait_click(dom, 'anaconda_begin_btn')
     click(dom, 'anaconda_done_btn')
-    waitclick(dom, 'anaconda_done_btn')
+    wait_click(dom, 'anaconda_done_btn')
     wait(dom, 'anaconda_reboot_btn', 3600)
     click(dom, 'anaconda_reboot_btn')
+    '''
 
-    logging.debug('Waiting for domain to shutdown')
+    print('Waiting for installation to finish')
+    while True:
+        wait_click_text(dom, 'reboot')
+
+    print('Waiting for domain to shutdown')
     while dom.is_running():
-        logging.debug('.')
+        print('.')
         time.sleep(1)
 
     print dom.is_running()
     time.sleep(1)
-    logging.debug('Starting again')
+    print('Starting again')
     print dom.is_running()
     dom.start()
 
+
 def write(dom, text):
     for letter in text:
-        dom.send_key(letter)
+        dom.key_down(letter)
+
 
 def firstboot(dom):
-    waitclick(dom, 'firstboot_forward_btn')
-    waitclick(dom, 'firstboot_forward_btn') # glitch
-    waitclick(dom, 'firstboot_forward_btn') # license
+    wait_click(dom, 'firstboot_forward_btn')
+    wait_click(dom, 'firstboot_forward_btn')
+    wait_click(dom, 'firstboot_forward_btn')
     wait(dom, 'firstboot_create_user_label')
     write(dom, 'pllm framework')
-    dom.send_key('tab')
+    dom.key_down('tab')
     write(dom, 'pllm')
-    dom.send_key('tab')
-    dom.send_key('tab')
+    dom.key_down('tab')
+    dom.key_down('tab')
     write(dom, 'a')
-    dom.send_key('tab')
+    dom.key_down('tab')
     write(dom, 'a')
-    waitclick(dom, 'firstboot_forward_btn')
-    waitclick(dom, 'firstboot_finish_btn')
+    wait_click(dom, 'firstboot_forward_btn')
+    wait_click(dom, 'firstboot_finish_btn')
 
-def f18(dom):
+
+def f20(dom):
     expect(dom, [
         ('grub', 'grub_autoboot_label', grub),
-        ('anaconda', 'anaconda_installation_label', anaconda),
-        ('firstboot', 'firstboot_welcome_label', firstboot),
-        ])
+    ])
+
+    expect_text(dom, [
+        ('anaconda', 'welcome to fedora', anaconda)
+    ])
 
 try:
     while True:
-        logging.debug('f18')
-        f18(dom)
-        time.sleep(.2)
+        print('f20')
+        f20(dom)
+        time.sleep(1)
 except KeyboardInterrupt:
     pass
