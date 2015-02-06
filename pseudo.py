366ce0c2af67034151f79d912fde03bd1e345b4c
Richard Marko <rmarko@fedoraproject.org>
2015-02-06 20:28:05 +0100

    update pseudo

--- pseudo.py
+++ pseudo.py
@@ -2,7 +2,8 @@ import time
 
 from pllm.vision.tasks import template_match
 
-PASSWORD = "1337password_is_leet!!!"
+PASSWORD = "fedora"
+PASSWORD_SIMPLE = True
 
 # useless
 TIMEOUT = 60000
@@ -99,8 +100,8 @@ def click(dom, template):
 def click_segment(dom, text, exact=False):
     print('Looking for segment with text "{0}"'.format(text))
 
-    for segname, data in dom.segments.items():
-        rect, ocrd = data
+    for ocrd, data in dom.segments.items():
+        rect, segname = data
         x, y, w, h = rect
 
         found = False
@@ -141,8 +142,8 @@ def wait_segment(dom, text, exact=False, timeout_seconds=TIMEOUT):
     print('Waiting segment with text "{0}"'.format(text))
 
     while True:
-        for segname, data in dom.segments.items():
-            rect, ocrd = data
+        for ocrd, data in dom.segments.items():
+            rect, segname = data
             x, y, w, h = rect
             found = False
 
@@ -155,7 +156,7 @@ def wait_segment(dom, text, exact=False, timeout_seconds=TIMEOUT):
 
             if found:
                 print('Wait target found at segment {0}x{1} after {2:.2f} sec'
-                     .format(x, y, time.time() - start))
+                      .format(x, y, time.time() - start))
                 return True
 
         time.sleep(0.1)
@@ -176,8 +177,8 @@ def wait_text(dom, text, timeout_seconds=TIMEOUT):
             return True
 
         # check segments
-        for segname, data in dom.segments.items():
-            rect, ocrd = data
+        for ocrd, data in dom.segments.items():
+            rect, segname = data
             x, y, w, h = rect
 
             if text.lower() in ocrd.lower():
@@ -206,6 +207,12 @@ def wait_next(dom, timeout_seconds=TIMEOUT):
     return True
 
 
+def wait_new_text(dom):
+    last = dom.text
+    while last == dom.text:
+        time.sleep(0.1)
+
+
 def wait_click(dom, template):
     wait(dom, template)
     click(dom, template)
@@ -219,9 +226,9 @@ def wait_click_text(dom, text, exact=False):
 def grub(dom):
     dom.key_press('tab')
     # erase part of cmdline
-    for _ in "rd.live.check quiet":
-        dom.key_press('backspace')
-        time.sleep(0.2)
+    #for _ in "rd.live.check quiet":
+    #    dom.key_press('backspace')
+    #    time.sleep(0.2)
 
     # disable gelocation so we don't start with random language
     dom.write(' geoloc=0')
@@ -230,19 +237,15 @@ def grub(dom):
 
 
 def anaconda(dom):
-    dom.mouse_move(1,1)
+    dom.mouse_move(1, 1)
 
     wait_click_text(dom, 'continue')
-    if not wait_text(dom, 'installation summary'):
-        print('First click glitch present')
-        wait_click_text(dom, 'continue')
 
     wait_text(dom, 'installation summary')
 
-    while True:
-        wait_click(dom, 'anaconda_storage_incomplete_btn')
-        if wait_text(dom, 'local standard disks'):
-            break
+    wait_click(dom, 'anaconda_storage_incomplete_btn')
+
+    wait_text(dom, 'local standard disks')
 
     wait_click_text(dom, 'I will configure partitioning')
     wait_click_text(dom, 'done')
@@ -255,21 +258,36 @@ def anaconda(dom):
         dom.key_press('right')
         dom.key_press('tab')
         dom.key_press('tab')
+        time.sleep(0.1)
         dom.write(size)
 
+        #wait_click_text(dom, 'add mount point')
         wait_click_text(dom, 'add mount point')
+        wait_text(dom, 'reset all')
 
-    create_partition('/', '5G')
-    create_partition('/boot', '200M')
-    create_partition('swap', '1G')
-    create_partition('/home', '')
+    def create_layout():
+        create_partition('/', '5G')
+        create_partition('/boot', '200M')
+        create_partition('swap', '1G')
+        create_partition('/home', '')
+        wait_click_text(dom, 'done')
 
-    wait_click_text(dom, 'done')
+    create_layout()
+
+    wait_new_text(dom)
+    warn_str = "Click for details or press Done again to continue."
+    if warn_str in dom.text:
+        print("SHOULD RESET!")
+        wait_click_text(dom, 'reset all')
+        create_layout()
+
     wait_click_text(dom, 'accept changes')
 
-    wait_click(dom, 'anaconda_software_btn')
-    #wait_click(dom, 'anaconda_xfce_choice')
-    wait_click_text(dom, 'done')
+    def sw_selection():
+        wait_click(dom, 'anaconda_software_btn')
+        wait_click_text(dom, 'Fedora Workstation', exact=True)
+        wait_click_text(dom, 'done')
+
     wait_click_text(dom, 'begin installation')
 
     wait_click_text(dom, 'root password')
@@ -279,6 +297,11 @@ def anaconda(dom):
         dom.key_press('tab')
 
     wait_click_text(dom, 'done')
+    if PASSWORD_SIMPLE:
+        # can detect
+        # The password you have provided is weak
+        wait_next(dom)
+        wait_click_text(dom, 'done')
 
     time.sleep(2)
     print(dom.text)
@@ -296,13 +319,25 @@ def anaconda(dom):
         dom.write(PASSWORD)
         dom.key_press('tab')
 
+    if PASSWORD_SIMPLE:
+        wait_next(dom)
+        wait_click_text(dom, 'done')
+
     wait_click_text(dom, 'done')
+    print('Waiting for installation to finish')
+    wait_segment(dom, "Complete!")
 
-    wait_next(dom)
-    print(dom.text)
+    shall_quit_shell = False
+    if "Reboot" in dom.segments:
+        wait_click_text(dom, 'Reboot', exact=True)
+    elif "Quit" in dom.segments:
+        wait_click_text(dom, 'Quit', exact=True)
+        shall_quit_shell = True
 
-    print('Waiting for configuration to finish')
-    wait_click_text(dom, 'Reboot', exact=True)
+    if shall_quit_shell:
+        wait_click(dom, 'shell_power_button')
+        wait_click(dom, 'shell_power_button_2')
+        wait_click(dom, 'shell_restart_button')
 
     print('Waiting for domain to shutdown')
     while dom.is_running():
@@ -315,16 +350,50 @@ def anaconda(dom):
     print dom.is_running()
     dom.start()
 
+
+def live_selector(dom):
+    dom.mouse_move(1, 1)
+    wait_click_text(dom, "install to hard drive")
+    wait_text(dom, "installation")
+    anaconda(dom)
+
+
+def gdm(dom):
+    dom.mouse_move(1, 1)
+    dom.key_press('ret')
+    dom.write(PASSWORD)
+    dom.key_press('ret')
+
+
+def gnome_initial_setup(dom):
+    dom.mouse_move(1, 1)
+    wait_click_text(dom, "next")
+    wait_next(dom)
+    wait_click_text(dom, "next")
+    wait_next(dom)
+    wait_click_text(dom, "skip")
+    wait_next(dom)
+    wait_click_text(dom, "start using fedora")
+
+
 def f21(dom):
     expect(dom, [
-        ('grub', 'grub_autoboot_label', grub),
+        #('grub', 'grub_autoboot_label', grub),
+        ('grub', 'grub_press_tab', grub),
+        ('gdm', 'login_logo', gdm),
     ])
 
     expect_text(dom, [
-        ('anaconda', 'welcome to fedora', anaconda)
+        ('anaconda', 'installation', anaconda),
+        ('live_selector', 'install to hard drive', live_selector),
+        ('gnome-initial-setup', 'Gnome-initial-setup', gnome_initial_setup),
     ])
 
 try:
+    while dom.screen is None:
+        time.sleep(0.1)
+        print('.')
+
     while True:
         print('f21')
         f21(dom)
